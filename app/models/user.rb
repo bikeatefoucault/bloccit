492ee7175d06ed227da46756dbeea347a7b2bd9e
bikeatefoucault <bikeatefoucault@gmail.com>
2014-01-29 07:17:43 -0500

    this should add fb auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,17 +14,17 @@ class User < ActiveRecord::Base
 
   mount_uploader :avatar, AvatarUploader
 
-  def self.find_for_facebook_oath(auth, signed_in_resource=nil)
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
       pass = Devise.friendly_token[0,20]
       user = User.new(name: auth.extra.raw_info.name,
-                          provider: auth.provider,
-                          uid: auth.uid,
-                          email: auth.info.email,
-                          password: pass,
-                          password_confirmation: pass
-                      )
+                         provider: auth.provider,
+                         uid: auth.uid,
+                         email: auth.info.email,
+                         password: pass,
+                         password_confirmation: pass
+                        )
       user.skip_confirmation!
       user.save
     end
